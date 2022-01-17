c7573fcd8d17b238ee9b7ca530dde3d1feff2cc9
Kaique <kaiquevan@hotmail.com>
2022-01-17 18:42:48 -0300

    att searchId  method

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -15,15 +15,6 @@ class TransactionController{
             
             const transaction = await transactionService.searchId(id as string);
 
-            if(transaction == null){
-                
-                return response.status(404).json({
-                    status: transaction.status,
-                    message: "Invalid account number"
-                });
-            }
-
-
             return response.status(201).json({
                 status: transaction.status,
             });
--- src/service/TransactionService.ts
+++ src/service/TransactionService.ts
@@ -1,6 +1,7 @@
 import { ITransactionRepository } from '../repositories/ITransactionRepository';
 import { AppError } from '../erros/AppError';
 import Transaction, { TransactionDocument } from '../model/Transaction';
+import { TransactionRepository } from '../repositories/TransactionRepository';
 
 interface IRequest{
     transactionId?: string;
@@ -10,11 +11,20 @@ interface IRequest{
 
 }
 
+const  transactionRepository = new TransactionRepository();
+
 class TransactionService{
 
     constructor(private transactionRepository: ITransactionRepository ){ }
 
     async searchId(id:string): Promise<TransactionDocument>{
+
+        const transactionVoid =  await this.transactionRepository.searchId(id);
+
+        if (!transactionVoid){
+            throw new AppError("Invalid account number");
+        }
+
         return await this.transactionRepository.searchId(id);   
     }
 
